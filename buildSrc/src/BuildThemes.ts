e1c3f1a487834618923f59841f1dd62fbf7a1be2
DATBOI <alexsimons9999@gmail.com>
2022-02-13 11:41:52 -0600

    Able to build Doki definitions now.

--- buildSrc/src/BuildThemes.ts
+++ buildSrc/src/BuildThemes.ts
@@ -4,10 +4,11 @@ import {
   DokiThemeDefinitions,
   evaluateTemplates,
   MasterDokiThemeDefinition,
+  resolveColor,
   resolvePaths,
-  StringDictionary,
+  StringDictionary
 } from "doki-build-source";
-import omit from 'lodash/omit';
+import omit from "lodash/omit";
 import fs from "fs";
 import path from "path";
 
@@ -18,17 +19,40 @@ const {
   masterThemeDefinitionDirectoryPath,
 } = resolvePaths(__dirname);
 
-// todo: dis
-type DokiThemeJupyter = {
-  [k: string]: any;
-};
+type DokiThemeFirefox = BaseAppDokiThemeDefinition;
+
+function buildColors(
+  dokiThemeDefinition: MasterDokiThemeDefinition,
+  dokiTemplateDefinitions: DokiThemeDefinitions,
+  dokiThemeChromeDefinition: DokiThemeFirefox,
+): StringDictionary<string> {
+  const namedColors = constructNamedColorTemplate(
+    dokiThemeDefinition, dokiTemplateDefinitions
+  )
+  const themeOverrides = dokiThemeChromeDefinition.overrides.theme &&
+    dokiThemeChromeDefinition.overrides.theme.colors || {};
+  const colorsOverride: StringDictionary<string> = {
+    ...namedColors,
+    ...themeOverrides,
+    ...dokiThemeChromeDefinition.colors,
+    editorAccentColor: dokiThemeDefinition.overrides?.editorScheme?.colors.accentColor ||
+      dokiThemeDefinition.colors.accentColor
+  }
+  return Object.entries<string>(colorsOverride).reduce(
+    (accum, [colorName, colorValue]) => ({
+      ...accum,
+      [colorName]: resolveColor(colorValue, namedColors),
+    }),
+    {}
+  );
+}
 
 
 function buildTemplateVariables(
   dokiThemeDefinition: MasterDokiThemeDefinition,
   masterTemplateDefinitions: DokiThemeDefinitions,
   dokiThemeAppDefinition: AppDokiThemeDefinition,
-): DokiThemeJupyter {
+): StringDictionary<string> {
   const namedColors: StringDictionary<string> = constructNamedColorTemplate(
     dokiThemeDefinition,
     masterTemplateDefinitions
@@ -46,24 +70,38 @@ function buildTemplateVariables(
   };
 }
 
+interface FireFoxDokiThemeBuild {
+  path: string,
+  definition: MasterDokiThemeDefinition | {colors: StringDictionary<string>},
+  stickers: Stickers,
+  templateVariables: StringDictionary<string>
+  appThemeDefinition: DokiThemeFirefox
+}
+
 function createDokiTheme(
   masterThemeDefinitionPath: string,
   masterThemeDefinition: MasterDokiThemeDefinition,
   appTemplateDefinitions: DokiThemeDefinitions,
-  appThemeDefinition: AppDokiThemeDefinition,
+  appThemeDefinition: DokiThemeFirefox,
   masterTemplateDefinitions: DokiThemeDefinitions,
-) {
+): FireFoxDokiThemeBuild {
   try {
     return {
       path: masterThemeDefinitionPath,
-      definition: masterThemeDefinition,
+      definition: {
+        ...masterThemeDefinition,
+        colors: buildColors(
+          masterThemeDefinition,
+          masterTemplateDefinitions,
+          appThemeDefinition
+        )
+      },
       stickers: getStickers(masterThemeDefinition, masterThemeDefinitionPath),
       templateVariables: buildTemplateVariables(
         masterThemeDefinition,
         masterTemplateDefinitions,
         appThemeDefinition,
       ),
-      theme: {},
       appThemeDefinition: appThemeDefinition,
     };
   } catch (e) {
@@ -83,7 +121,8 @@ function resolveStickerPath(themeDefinitionPath: string, sticker: string) {
   );
 }
 
-const getStickers = (
+type Stickers = { default: { path: string; name: string } };
+const getStickers: (dokiDefinition: MasterDokiThemeDefinition, themePath: string) => Stickers = (
   dokiDefinition: MasterDokiThemeDefinition,
   themePath: string
 ) => {
@@ -110,32 +149,31 @@ const themesDirectory = path.resolve(repoDirectory, "src", "dokithemejupyter");
 
 evaluateTemplates(
   {
-    appName: 'jupyter',
+    appName: 'firefox',
     currentWorkingDirectory: __dirname,
   },
   createDokiTheme
 )
   .then((dokiThemes) => {
-
     // write things for extension
-    const dokiThemeDefinitions = dokiThemes
-      .map((dokiTheme) => {
-        const dokiDefinition = dokiTheme.definition;
-        return {
-          information: omit(dokiDefinition, [
-            "colors",
-            "overrides",
-            "ui",
-            "icons",
+    const dokiThemeDefinitions = dokiThemes.map(dokiTheme => {
+      const dokiDefinition = dokiTheme.definition;
+      return {
+        information: {
+          ...omit(dokiDefinition, [
+            'colors',
+            'overrides',
+            'ui',
+            'icons'
           ]),
-          colors: dokiTheme.appThemeDefinition.colors,
-          stickers: dokiTheme.stickers,
-        };
-      })
-      .reduce((accum: StringDictionary<any>, definition) => {
-        accum[definition.information.id] = definition;
-        return accum;
-      }, {});
+        },
+        colors: dokiDefinition.colors,
+        overrides: dokiTheme.appThemeDefinition.overrides
+      };
+    }).reduce((accum: StringDictionary<any>, definition: any) => {
+      accum[definition.information.id] = definition;
+      return accum;
+    }, {});
     const finalDokiDefinitions = JSON.stringify(dokiThemeDefinitions);
     fs.writeFileSync(
       path.resolve(repoDirectory, "src", "DokiThemeDefinitions.ts"),
