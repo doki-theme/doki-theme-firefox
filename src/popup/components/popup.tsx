d57b48a3e4f1e0cd9451197cb3bc287ebb3d927a
DATBOI <alexsimons9999@gmail.com>
2022-02-14 17:56:18 -0600

    Added themed select component.

--- src/popup/components/popup.tsx
+++ src/popup/components/popup.tsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { FC, useEffect } from "react";
 import { ThemeContext } from "../../themes/DokiThemeProvider";
 import Select from "react-select";
+import { DokiTheme } from "../../themes/DokiTheme";
+import ThemedSelect from "./ThemedSelect";
 
-const options = [
+const options: any[] = [
   { value: "chocolate", label: "Chocolate" },
   { value: "strawberry", label: "Strawberry" },
   { value: "vanilla", label: "Vanilla" }
 ];
 
+const ThemeStuff: FC<{ theme: DokiTheme }> = ({ theme }) => {
+  useEffect(() => {
+    document.documentElement.style.setProperty("--base-background", theme.colors.baseBackground);
+  }, [theme]);
+  return <></>;
+};
 
 const Popup = () => {
+
   return (
     <ThemeContext.Consumer>
       {
@@ -19,64 +28,16 @@ const Popup = () => {
             backgroundColor: colors.baseBackground,
             color: colors.lineNumberColor,
             padding: "1rem",
-            minHeight: '500px',
+            minHeight: "500px",
+            minWidth: "250px"
           }}>
+            <ThemeStuff theme={theme}></ThemeStuff>
             <header>
               <h1 style={{ margin: "0 0 1rem 0" }}>Doki Theme</h1>
             </header>
 
-            Current Theme: {theme.themeId}
-            <Select options={options}
-                    theme={(theme) => ({
-                      ...theme,
-                      colors: {
-                        ...theme.colors,
-                        primary: colors.editorAccentColor,
-                        neutral50: colors.infoForeground,
-                        neutral10: colors.borderColor
-                      }
-                    })}
-                    styles={{
-                      control: (styles) => ({
-                        ...styles,
-                        backgroundColor: colors.buttonColor,
-                        borderColor: colors.borderColor,
-                        ":hover": {
-                          ...styles[":hover"],
-                          borderColor: colors.editorAccentColor + "99"
-                        }
-                      }),
-                      option: (provided, state) => ({
-                        ...provided,
-                        borderBottom: `1px dotted ${colors.infoForeground}`,
-                        color: state.isSelected ? colors.selectionForeground : colors.foregroundColorEditor,
-                        ':hover': {
-                          ...provided[':hover'],
-                          backgroundColor: colors.selectionBackground,
-                          color: colors.selectionForeground,
-                        },
-                        ':active': {
-                          ...provided[':active'],
-                          backgroundColor: colors.selectionBackground,
-                          color: colors.selectionForeground,
-                        }
-                      }),
-                      menu: (styles) => ({
-                        ...styles,
-                        backgroundColor: colors.lightEditorColor,
-                        borderColor: colors.borderColor,
-                      }),
-                      indicatorSeparator: (styles) => ({
-                        ...styles,
-                        backgroundColor: colors.baseIconColor,
-                      }),
-                      dropdownIndicator: (styles) => ({
-                        ...styles,
-                        color: colors.baseIconColor,
-                        borderColor: colors.borderColor
-                      })
-
-                    }} />
+            <ThemedSelect options={options}
+                          defaultValue={options[0]} />
           </div>;
         }
       }
