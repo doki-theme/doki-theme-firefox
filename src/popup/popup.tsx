caaea40a43fe6c84661c674419f4131d1e351f0e
Alex Simons <alexsimons9999@gmail.com>
2022-04-16 17:21:17 -0500

    Updated React to 18 (#66)
    * Updated React to 18
    
    * Updated changelog & bump version
    
    * Updated deps
    
    * Updated deps v2

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,17 +1,18 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import Popup from "./components/popup";
 import DokiThemeProvider from "../common/DokiThemeProvider";
 import FeatureProvider from "../common/FeatureProvider";
 import "react-tabs/style/react-tabs.css";
 
 browser.tabs.query({ active: true, currentWindow: true }).then(() => {
-  ReactDOM.render(
+  const container = document.getElementById("popup");
+  const root = createRoot(container!);
+  root.render(
     <DokiThemeProvider>
       <FeatureProvider>
         <Popup />
       </FeatureProvider>
-    </DokiThemeProvider>,
-    document.getElementById("popup")
+    </DokiThemeProvider>
   );
 });
